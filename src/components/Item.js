c12ec46352f5fe280043e0020a62f72429fa8b2b
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-03 08:39:18 +0300

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function Item({ name, category }) {
+  //adding function to toggle between adding to cart and removing from cart
+  const [inCart, setInCart] = React.useState(false)
+  
+  function handleCart() {
+      setInCart(previousCart => !previousCart)
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{inCart ? "Remove from cart" : "Add to cart"}</button>
     </li>
   );
 }
